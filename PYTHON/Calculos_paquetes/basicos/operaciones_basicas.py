0137a517cf5f497ddbbbc85b4e46f5d9b762e49b
Julian Valencia <valenciajulian657@gmail.com>
2022-08-23 17:29:24 -0500

    Paquetes y subpaquetes

---
+++ PYTHON/Calculos_paquetes/basicos/operaciones_basicas.py
@@ -0,0 +1,11 @@
+def sumar(num1, num2):
+    print(f"El resultado de la suma es: {num1+num2}")
+
+def restar(num1, num2):
+    print(f"El resultado de la resta es: {num1-num2}")
+
+def multiplicar(num1, num2):
+    print(f"El resultado de la multiplicación es: {num1*num2}")
+
+def dividir(dividendo, divisor):
+    print(f"El resultado de la división es: {dividendo/divisor}")
---
+++ PYTHON/Calculos_paquetes/calculos_generales.py
@@ -0,0 +1,17 @@
+def sumar(num1, num2):
+    print(f"El resultado de la suma es: {num1+num2}")
+
+def restar(num1, num2):
+    print(f"El resultado de la resta es: {num1-num2}")
+
+def multiplicar(num1, num2):
+    print(f"El resultado de la multiplicación es: {num1*num2}")
+
+def dividir(dividendo, divisor):
+    print(f"El resultado de la división es: {dividendo/divisor}")
+
+def potencia(base, exponente):
+    print(f"El resultado de la potencia es: {base**exponente}")
+
+def redondear(numero):
+    print(f"El resultado es: {round(numero)}")

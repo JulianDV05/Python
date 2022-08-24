5c591569e6429df98bed22864873fd02f29c1b7a
Julian Valencia <valenciajulian657@gmail.com>
2022-08-24 16:56:57 -0500

    Creación, instalación y eliminación de paquetes distribuibles

---
+++ PYTHON/Modulos/usoModulosDistribuibles.py
@@ -0,0 +1,10 @@
+#from Calculos_paquetes.calculos_generales import dividir, potencia
+
+#dividir(4,6)
+#potencia(4,6)
+
+from Calculos_paquetes.basicos.operaciones_basicas import *
+from Calculos_paquetes.redondeo_potencia.redondeaYpotencia import *
+
+sumar(5,7)
+potencia(5,7)
---
+++ PYTHON/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+
+    name="paquetecalculos",
+    version="1.0",
+    description="paquete de redondeo y potencia",
+    author="Julian Valencia",
+    author_email="valenciajulian657@gmail.com",
+    url="www.pildorasinformaticas.es",
+    packages=["Calculos_paquetes", "Calculos_paquetes.redondeo_potencia"]
+)
